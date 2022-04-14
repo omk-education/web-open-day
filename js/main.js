1dbeb5c712651f5bde159f423cd746ff2bed15e9
Leonov Ivan <it.omk@yandex.ru>
2022-04-14 21:59:04 +0300

    add info

--- js/main.js
+++ js/main.js
@@ -17,43 +17,46 @@ document.addEventListener('click', startGame, { once: true });
  *
  * @return {undefined}
  */
-function startGame() {
-  // обнуляем очки
-  point = 0;
-  // скрываем началную надпись
-  startScreen.hidden = true;
-  // запускаем анимацию кактуса
-  cactus.classList.add('motion');
-  // запускаем анимацию земли
-  ground.classList.add('slide');
-  // меняем изображение на бегущего динозвра
-  dino.src = 'images/dino-run.gif';
-  // начинаем обрабатывать прыжок динозавра
-  document.addEventListener('click', jump);
-  // цикл для подсчета очков
-  scoringPoints = setInterval(() => {
-    score.innerHTML = point++;
-  }, 500);
+function startGame(event) {
+  // обрабатываем только клики внутри игрового поля
+  if (event.target.closest('.game')) {
+    // обнуляем очки
+    point = 0;
+    // скрываем началную надпись
+    startScreen.hidden = true;
+    // запускаем анимацию кактуса
+    cactus.classList.add('motion');
+    // запускаем анимацию земли
+    ground.classList.add('slide');
+    // меняем изображение на бегущего динозвра
+    dino.src = 'images/dino-run.gif';
+    // начинаем обрабатывать прыжок динозавра
+    document.addEventListener('click', jump);
+    // цикл для подсчета очков
+    scoringPoints = setInterval(() => {
+      score.innerHTML = point++;
+    }, 200);
 
-  // запускам цикл для обработки столкновений
-  isAlive = setInterval(() => {
-    // получаем координаты динозавра
-    let dinoTop = parseInt(
-      window.getComputedStyle(dino).getPropertyValue('top')
-    );
-    //  получаем координаты кактуса
-    let cactusLeft = parseInt(
-      window.getComputedStyle(cactus).getPropertyValue('left')
-    );
+    // запускам цикл для обработки столкновений
+    isAlive = setInterval(() => {
+      // получаем координаты динозавра
+      let dinoTop = parseInt(
+        window.getComputedStyle(dino).getPropertyValue('top')
+      );
+      //  получаем координаты кактуса
+      let cactusLeft = parseInt(
+        window.getComputedStyle(cactus).getPropertyValue('left')
+      );
 
-    // условие столкновения кактуса и динозвра
-    let isCollision = cactusLeft > 0 && cactusLeft < 50 && dinoTop >= 140;
+      // условие столкновения кактуса и динозвра
+      let isCollision = cactusLeft > 0 && cactusLeft < 50 && dinoTop >= 140;
 
-    // проверка на столкновение
-    if (isCollision) {
-      gameOver();
-    }
-  }, 10);
+      // проверка на столкновение
+      if (isCollision) {
+        gameOver();
+      }
+    }, 10);
+  }
 }
 
 /**
@@ -81,14 +84,18 @@ function gameOver() {
 }
 
 // прыжок динозавра
-function jump() {
-  if (!dino.classList.contains('jump')) {
-    // прыгаем через добавление класса
-    dino.classList.add('jump');
+function jump(event) {
+  // обрабатываем только клики внутри игрового поля
+  if (event.target.closest('.game')) {
+    // проверяем наличие класса jump
+    if (!dino.classList.contains('jump')) {
+      // прыгаем через добавление класса
+      dino.classList.add('jump');
 
-    // удаляем класс через 300 мс
-    setTimeout(() => {
-      dino.classList.remove('jump');
-    }, 300);
+      // удаляем класс через 300 мс
+      setTimeout(() => {
+        dino.classList.remove('jump');
+      }, 300);
+    }
   }
 }
